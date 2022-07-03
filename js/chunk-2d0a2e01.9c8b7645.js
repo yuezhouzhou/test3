2d5fce75fe4018fbbaf478a1149625c24490694d
wanglei..A <1272093759@qq.com>
2022-07-03 20:50:13 +0800

    first commit

---
+++ js/chunk-2d0a2e01.9c8b7645.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0a2e01"],{"007b":function(e,n,t){"use strict";t.r(n);var u=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[e._v(" video1 ")])},c=[],l=t("2877"),a={},i=Object(l["a"])(a,u,c,!1,null,null,null);n["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-2d0a2e01.9c8b7645.js.map
---
+++ js/chunk-2d0a4772.3a4356f8.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0a4772"],{"070e":function(n,e,t){"use strict";t.r(e);var u=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[n._v(" question ")])},s=[],a={name:"Question"},c=a,i=t("2877"),l=Object(i["a"])(c,u,s,!1,null,null,null);e["default"]=l.exports}}]);
+//# sourceMappingURL=chunk-2d0a4772.3a4356f8.js.map
